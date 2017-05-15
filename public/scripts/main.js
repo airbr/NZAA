0b0f2c7660c73bf42752ce2062b033c09d230c6a
Morgan Murrah <mmurrah@airbridge.ac.nz>
2017-05-15 16:02:05 -0400

    Cleaning up Google Docs HTML before Vue

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,14 +3,6 @@
 
     var SERVER_URL = 'https://new-zealand-atlanta-assn.firebaseio.com/api.json';
 
-    //jQuery Implementation
-    // $.ajax({url: SERVER_URL, success: function(result){
-    //     console.log(result);
-    //     var main = JSON.stringify(result);
-    //
-    //     $('#div1').html(main);
-    // }});
-
     //fetch implementation
     function timeToFetch() {
         fetch('https://new-zealand-atlanta-assn.firebaseio.com/api.json').then(function (response) {
@@ -18,19 +10,17 @@
             return response.json();
         }).then(function (data) {
 
-                var main = JSON.stringify(data);
+            var raw = JSON.stringify(data[0].content);
+            // Remove Ugly Quotes from beginning and end of doc- TODO: clean up entire HTML from Google Docs
+            var main = raw.slice(0, -1).substring(1);
 
-                // $('#div1').html(main);
-
-                var app = new Vue({
-                    el: '#app',
-                    data: {
-                        message: main
-                    }
-                })
-
-
-            return console.log(data);
+            var app = new Vue({
+                el: '#app',
+                data: {
+                    message: main
+                }
+            })
+            return console.log(main);
         }).catch(function (error) {
             return console.log('error is', error);
         });
