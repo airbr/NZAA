522cd698b408259ce77aca7c7b288d173e2570cd
Morgan Murrah <mmurrah@airbridge.ac.nz>
2017-05-15 10:03:18 -0400

    Testing Fetch API, removal of jQuery for GET

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,12 +3,29 @@
 
     var SERVER_URL = 'https://new-zealand-atlanta-assn.firebaseio.com/api.json';
 
-    $.ajax({url: SERVER_URL, success: function(result){
+    // $.ajax({url: SERVER_URL, success: function(result){
+    //     console.log(result);
+    //     var main = JSON.stringify(result);
+    //
+    //     $('#div1').html(main);
+    // }});
 
-        var main = JSON.stringify(result);
-        $('#div1').html(main);
-    }});
+    function timeToFetch() {
+        fetch('https://new-zealand-atlanta-assn.firebaseio.com/api.json').then(function (response) {
+            console.log(response);
+            return response.json();
+        }).then(function (data) {
 
+                var main = JSON.stringify(data);
+
+                $('#div1').html(main);
+
+            return console.log(data);
+        }).catch(function (error) {
+            return console.log('error is', error);
+        });
+    }
+    timeToFetch();
 
 })();
 
